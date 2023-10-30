cae718660fced632b09808d24d00cf6de6356dce
vijay-10 <vijayvardhansn10@gmail.com>
2023-10-30 21:49:05 +0530

    changing title

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -3,53 +3,53 @@ import React from 'react'
 const Shimmer = () => {
   return (
     <div className='flex justify-between flex-wrap gap-7'>
-        <div className='my-4 p-4 w-64 h-72 rounded-lg bg-slate-100 shadow-lg flex flex-col gap-1 animate-pulse leading-relaxed'>
-            <div className='w-[90%] h-[40%] bg-white mx-auto rounded-lg animate-pulse leading-relaxed'></div>
-            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse leading-relaxed'></div>
-            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse leading-relaxed'></div>
-            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse leading-relaxed'></div>
+        <div className='my-4 p-4 w-64 h-72 rounded-lg bg-slate-100 shadow-lg flex flex-col gap-1 animate-pulse'>
+            <div className='w-[90%] h-[40%] bg-white mx-auto rounded-lg animate-pulse'></div>
+            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse'></div>
+            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse'></div>
+            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse'></div>
         </div>
-        <div className='my-4 p-4 w-64 h-72 rounded-lg bg-slate-100 shadow-lg flex flex-col gap-1 animate-pulse leading-relaxed'>
-            <div className='w-[90%] h-[40%] bg-white mx-auto rounded-lg animate-pulse leading-relaxed'></div>
-            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse leading-relaxed'></div>
-            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse leading-relaxed'></div>
-            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse leading-relaxed'></div>
+        <div className='my-4 p-4 w-64 h-72 rounded-lg bg-slate-100 shadow-lg flex flex-col gap-1 animate-pulse'>
+            <div className='w-[90%] h-[40%] bg-white mx-auto rounded-lg animate-pulse'></div>
+            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse'></div>
+            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse'></div>
+            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse'></div>
         </div>
-        <div className='my-4 p-4 w-64 h-72 rounded-lg bg-slate-100 shadow-lg flex flex-col gap-1 animate-pulse leading-relaxed'>
-            <div className='w-[90%] h-[40%] bg-white mx-auto rounded-lg animate-pulse leading-relaxed'></div>
-            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse leading-relaxed'></div>
-            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse leading-relaxed'></div>
-            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse leading-relaxed'></div>
+        <div className='my-4 p-4 w-64 h-72 rounded-lg bg-slate-100 shadow-lg flex flex-col gap-1 animate-pulse'>
+            <div className='w-[90%] h-[40%] bg-white mx-auto rounded-lg animate-pulse'></div>
+            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse'></div>
+            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse'></div>
+            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse'></div>
         </div>
-        <div className='my-4 p-4 w-64 h-72 rounded-lg bg-slate-100 shadow-lg flex flex-col gap-1 animate-pulse leading-relaxed'>
-            <div className='w-[90%] h-[40%] bg-white mx-auto rounded-lg animate-pulse leading-relaxed'></div>
-            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse leading-relaxed'></div>
-            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse leading-relaxed'></div>
-            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse leading-relaxed'></div>
+        <div className='my-4 p-4 w-64 h-72 rounded-lg bg-slate-100 shadow-lg flex flex-col gap-1 animate-pulse'>
+            <div className='w-[90%] h-[40%] bg-white mx-auto rounded-lg animate-pulse'></div>
+            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse'></div>
+            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse'></div>
+            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse'></div>
         </div>
-        <div className='my-4 p-4 w-64 h-72 rounded-lg bg-slate-100 shadow-lg flex flex-col gap-1 animate-pulse leading-relaxed'>
-            <div className='w-[90%] h-[40%] bg-white mx-auto rounded-lg animate-pulse leading-relaxed'></div>
-            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse leading-relaxed'></div>
-            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse leading-relaxed'></div>
-            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse leading-relaxed'></div>
+        <div className='my-4 p-4 w-64 h-72 rounded-lg bg-slate-100 shadow-lg flex flex-col gap-1 animate-pulse'>
+            <div className='w-[90%] h-[40%] bg-white mx-auto rounded-lg animate-pulse'></div>
+            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse'></div>
+            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse'></div>
+            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse'></div>
         </div>
-        <div className='my-4 p-4 w-64 h-72 rounded-lg bg-slate-100 shadow-lg flex flex-col gap-1 animate-pulse leading-relaxed'>
-            <div className='w-[90%] h-[40%] bg-white mx-auto rounded-lg animate-pulse leading-relaxed'></div>
-            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse leading-relaxed'></div>
-            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse leading-relaxed'></div>
-            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse leading-relaxed'></div>
+        <div className='my-4 p-4 w-64 h-72 rounded-lg bg-slate-100 shadow-lg flex flex-col gap-1 animate-pulse'>
+            <div className='w-[90%] h-[40%] bg-white mx-auto rounded-lg animate-pulse'></div>
+            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse'></div>
+            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse'></div>
+            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse'></div>
         </div>
-        <div className='my-4 p-4 w-64 h-72 rounded-lg bg-slate-100 shadow-lg flex flex-col gap-1 animate-pulse leading-relaxed'>
-            <div className='w-[90%] h-[40%] bg-white mx-auto rounded-lg animate-pulse leading-relaxed'></div>
-            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse leading-relaxed'></div>
-            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse leading-relaxed'></div>
-            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse leading-relaxed'></div>
+        <div className='my-4 p-4 w-64 h-72 rounded-lg bg-slate-100 shadow-lg flex flex-col gap-1 animate-pulse'>
+            <div className='w-[90%] h-[40%] bg-white mx-auto rounded-lg animate-pulse'></div>
+            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse'></div>
+            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse'></div>
+            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse'></div>
         </div>
-        <div className='my-4 p-4 w-64 h-72 rounded-lg bg-slate-100 shadow-lg flex flex-col gap-1 animate-pulse leading-relaxed'>
-            <div className='w-[90%] h-[40%] bg-white mx-auto rounded-lg animate-pulse leading-relaxed'></div>
-            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse leading-relaxed'></div>
-            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse leading-relaxed'></div>
-            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse leading-relaxed'></div>
+        <div className='my-4 p-4 w-64 h-72 rounded-lg bg-slate-100 shadow-lg flex flex-col gap-1 animate-pulse'>
+            <div className='w-[90%] h-[40%] bg-white mx-auto rounded-lg animate-pulse'></div>
+            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse'></div>
+            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse'></div>
+            <div className='w-[90%] h-5 rounded-md bg-white m-auto animate-pulse'></div>
         </div>
         
     </div>
