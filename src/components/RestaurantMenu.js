a157a106d11a318ac7130749361264c555835f56
vijay-10 <vijayvardhansn10@gmail.com>
2023-11-24 18:29:19 +0530

    made few more UI changes

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -8,7 +8,6 @@ import { useState } from "react";
 const RestaurantMenu = () => {
   const { resId } = useParams();
   const resInfo = useRestaurantMenu(resId);
-  console.log(resInfo);
 
   const [showIndex, setShowIndex] = useState(0);
 
