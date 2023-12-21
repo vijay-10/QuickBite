c57b5545df65602d2b1d7840a3917be4c5b0380e
vijaynvvr <vijayvardhansn10@gmail.com>
2023-12-21 19:34:52 +0530

    changing cors proxy

--- src/components/Body.js
+++ src/components/Body.js
@@ -17,8 +17,7 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      RESTAURANTS_API +
-        `lat=${location[0]}&lng=${location[1]}`
+      'https://corsproxy.org/?'+ encodeURIComponent(RESTAURANTS_API + `lat=${location[0]}&lng=${location[1]}`)
     );
     const json = await data.json();
     let resData = json?.data?.cards[2]?.card?.card?.gridElements
--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -11,7 +11,7 @@ const useRestaurantMenu = (resId) => {
 
   const fetchData = async () => {
     const data = await fetch(
-      (MENU_API + resId)
+      'https://corsproxy.org/?'+ encodeURIComponent(MENU_API + resId)
     );
     const json = await data.json();
     setResInfo(json?.data);
